c38d3a2643ca5f929c450ca958747ce5f0720a18
IDI Build <idibuild@walla.com>
2017-07-12 20:30:31 +0300

    More applicative changes

--- app/filters/upper-case-first.js
+++ app/filters/upper-case-first.js
@@ -1,7 +1,6 @@
 export default  function () {
     return function (input) {
-        //todo -check
-        return angular.isString(input) ? input.split(' ').map(function (ch) {
+        return isString(input) ? input.split(' ').map(function (ch) {
             return ch.charAt(0).toUpperCase() + ch.substring(1);}).join(' ') : input;
     };
 };
